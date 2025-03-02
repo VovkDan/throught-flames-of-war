362c97d30a1a543fda3cf7a03d15e406c010e5dd
VovkDan <schoollcomputerc1@gmail.com>
2025-03-02 11:39:35 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById("loginForm").addEventListener("submit", async (e) => {
+    e.preventDefault();
+    
+    const password = document.getElementById("password").value;
+
+    const response = await fetch("/.netlify/functions/login", {
+      method: "POST",
+      body: JSON.stringify({ password }),
+    });
+
+    const data = await response.json();
+    
+    if (response.ok) {
+      localStorage.setItem("token", data.token); // Сохраняем токен
+      window.location.href = "stranicapass.html"; // Перенаправляем
+    } else {
+      alert("Ошибка: " + data);
+    }
+  });
