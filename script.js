42557c21394135345433abb72673356c4928fc1a
VovkDan <schoollcomputerc1@gmail.com>
2025-03-02 17:08:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 document.getElementById("loginForm").addEventListener("submit", async (e) => {
   e.preventDefault();
-
+  
   const password = document.getElementById("password").value;
 
   try {
     const response = await fetch("/.netlify/functions/login", {
       method: "POST",
+      body: JSON.stringify({ password }),
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({ password: password }) // Отправляем только пароль
     });
 
     const data = await response.json();
-
+    
     if (response.ok) {
-      localStorage.setItem("token", data.token); // Сохраняем токен
+      localStorage.setItem("token", data.token);
       window.location.href = "stranicapass.html"; // Перенаправляем
     } else {
       alert("Ошибка: " + data.message);
