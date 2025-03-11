caae6273d06cffe9610555fde4f17183df2b5848
VovkDan <schoollcomputerc1@gmail.com>
2025-03-11 08:57:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 document.getElementById("loginForm").addEventListener("submit", async (e) => {
   e.preventDefault();
-  
   const password = document.getElementById("password").value;
-
   try {
     const response = await fetch("/.netlify/functions/login", {
       method: "POST",
@@ -11,9 +9,7 @@ document.getElementById("loginForm").addEventListener("submit", async (e) => {
         "Content-Type": "application/json",
       },
     });
-
     const data = await response.json();
-    
     if (response.ok) {
       localStorage.setItem("token", data.token);
       window.location.href = "stranicapass.html"; // Перенаправляем
@@ -23,4 +19,16 @@ document.getElementById("loginForm").addEventListener("submit", async (e) => {
   } catch (error) {
     alert("Ошибка при отправке запроса: " + error.message);
   }
+  window.addEventListener('scroll', () => {
+        if(window.scrollY > 1500) {
+            scrollBtn.style.display = 'block';
+        } else {
+            scrollBtn.style.display = 'none';
+        }
+    })
+   scrollBtn.addEventListener('click', () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth' // Плавная прокрутка
+        });
 });
